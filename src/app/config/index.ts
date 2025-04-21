0936b3353f39f34257191b08a0a121a781b402ba
jahidhowlader <jahidhowlader.info@gmail.com>
2025-04-21 19:07:27 +0600

    create function for upload image but not implement

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -5,6 +5,9 @@ interface IConfig {
     DATABASE_URL: string;
     BCRIPT_SALT: string;
     JWT_SECRET: string;
+    CLOUDINARY_CLOUD_NAME: string;
+    CLOUDINARY_API_KEY: string;
+    CLOUDINARY_API_SECRET: string;
 }
 
 dotenv.config();
@@ -13,7 +16,10 @@ const config: IConfig = {
     PORT: process.env.PORT as string,
     DATABASE_URL: process.env.DATABASE_URL as string,
     BCRIPT_SALT: process.env.BCRIPT_SALT as string,
-    JWT_SECRET: process.env.JWT_SECRET as string
+    JWT_SECRET: process.env.JWT_SECRET as string,
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME as string,
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY as string,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET as string,
 };
 
 export default config;
